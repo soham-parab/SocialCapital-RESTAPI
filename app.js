048994a5a32e82b862d58d9a76a31631f624d985
soham-parab <57428835+soham-parab@users.noreply.github.com>
2021-06-27 15:24:03 +0530

    cors

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const dotenv = require("dotenv");
 const loginRoute = require("./Routes/Login.route");
 const registerRoute = require("./Routes/Register.route");
 const postRoute = require("./Routes/Posts.route");
-
+const cors = require("cors");
 dotenv.config();
 app.use(express.json());
-
+app.use(cors());
 app.use("/login", loginRoute);
 app.use("/register", registerRoute);
 app.use("/posts", postRoute);

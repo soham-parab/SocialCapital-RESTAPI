9c0cd85f557bf44ff7171c8823e1abf1306c1d8b
soham-parab <57428835+soham-parab@users.noreply.github.com>
2021-07-08 19:30:57 +0530

    fixed auth header

--- Middlewares/verifyToken.js
+++ Middlewares/verifyToken.js
@@ -1,16 +1,31 @@
+// const jwt = require("jsonwebtoken");
+
+// module.exports = function (req, res, next) {
+//   const token = req.header("auth-token");
+//   if (!token) {
+//     return res.status(401).send("ACCESS DENIED");
+//   }
+
+//   try {
+//     const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+//     req.user = verified;
+//     next();
+//   } catch (error) {
+//     res.status(401).send("Invalid credentials");
+//   }
+// };
+
 const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
-  const token = req.header("auth-token");
-  if (!token) {
-    return res.status(401).send("ACCESS DENIED");
-  }
+  const token = req.headers.authorization;
+  if (!token) return res.status(401).send("Access Denied");
 
   try {
-    const verified = jwt.verify(token, process.env.TOKEN_SECRET);
-    req.user = verified;
+    const { _id } = jwt.verify(token, process.env.TOKEN_SECRET);
+    req.userId = _id;
     next();
-  } catch (error) {
-    res.status(401).send("Invalid credentials");
+  } catch (err) {
+    res.status(400).send("Invalid Token");
   }
 };
